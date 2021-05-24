e4021b585eece861b1051fed5adb1ee138bb7def
André <andrevictor50@gmail.com>
2021-05-24 10:24:55 -0300

    Initial commit

---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,21 @@
+import { ProductItem } from "./ProductItem";
+
+type SearchResultsProps = {
+  results: Array<{
+    id: number;
+    price: number;
+    title: string;
+  }>
+}
+
+export function SearchResults({ results }: SearchResultsProps) {
+  return (
+    <div>
+      {
+        results.map(result => (
+          <ProductItem product={result} />
+        ))
+      }
+    </div>
+  );
+}
